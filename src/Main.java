742bb8bc355eebc235ef48a876e99fc3e3b1d0ef
admin <zhazira9818@gmail.com>
2025-04-16 20:32:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Bob", 2, "buldog");
+
+//        System.out.println("name " + dog.getName());
+//        System.out.println("weight " + dog.getWeight());
+//        System.out.println("paroda " + dog.getParoda());
+//        dog.makeSound();
+
+        System.out.println(dog);
+    }
+}
